09f70b1defdef48bc8f4369ebaa2e150539f6f60
Henry Cheung <henrycheung@techstudio.design>
2021-10-30 10:41:50 +0800

    clean up FileDownload.ino example

--- examples/FileDownload/FileDownload.ino
+++ examples/FileDownload/FileDownload.ino
@@ -4,341 +4,217 @@
  *   https://github.com/bakercp/CRC32
  *   or from http://librarymanager/all#CRC32+checksum
  *
- * SimpleNB Getting Started guide:
- *   https://github.com/techstudio-design/SimpleNB/blob/master/README.md
- *
  * ATTENTION! Downloading big files requires of knowledge of
  * the SimpleNB internals and some modem specifics,
  * so this is for more experienced developers.
  *
  **************************************************************/
+// Select your modem: (See AllFunctions example for the definition of other modem)
+#define SIMPLE_NB_MODEM_SIM7080
 
-// Select your modem:
-#define SIMPLE_NB_MODEM_SIM7000
-// #define SIMPLE_NB_MODEM_SIM7000SSL
-// #define SIMPLE_NB_MODEM_SIM7080
-// #define SIMPLE_NB_MODEM_UBLOX
-// #define SIMPLE_NB_MODEM_SARAR4
-// #define SIMPLE_NB_MODEM_BG96
-// #define SIMPLE_NB_MODEM_XBEE
-// #define SIMPLE_NB_MODEM_SEQUANS_MONARCH
-
-// Set serial for debug console (to the Serial Monitor, default speed 115200)
-#define Serial Serial
-
-// Set serial for AT commands (to the module)
-// Use Hardware Serial on Mega, Leonardo, Micro
-#ifndef __AVR_ATmega328P__
-#define SerialAT Serial1
-
-// or Software Serial on Uno, Nano
-#else
-#include <SoftwareSerial.h>
-SoftwareSerial SerialAT(2, 3);  // RX, TX
-#endif
-
-// Increase RX buffer to capture the entire response
-// Chips without internal buffering (A6/A7, ESP8266, M590)
-// need enough space in the buffer for the entire response
-// else data will be lost (and the http library will fail).
-#if !defined(SIMPLE_NB_RX_BUFFER)
-#define SIMPLE_NB_RX_BUFFER 1024
-#endif
-
-// See all AT commands, if wanted
-// #define DUMP_AT_COMMANDS
-
-// Define the serial console for debug prints, if needed
-#define SIMPLE_NB_DEBUG Serial
-// #define LOGGING  // <- Logging is for the HTTP library
-
-// Add a reception delay, if needed.
-// This may be needed for a fast processor at a slow baud rate.
-// #define SIMPLE_NB_YIELD() { delay(2); }
-
-// Define how you're planning to connect to the internet.
-// This is only needed for this example, not in other code.
-#define SIMPLE_NB_USE_GPRS true
-#define SIMPLE_NB_USE_WIFI false
-
-// set GSM PIN, if any
-#define GSM_PIN ""
-
-// Your GPRS credentials, if any
-const char apn[]      = "YourAPN";
-const char gprsUser[] = "";
-const char gprsPass[] = "";
-
-// Your WiFi connection credentials, if applicable
-const char wifiSSID[] = "YourSSID";
-const char wifiPass[] = "YourWiFiPass";
-
-// Server details
-const char server[] = "vsh.pp.ua";
-const int  port     = 80;
+// Select your Serial port for AT interface (See AllFunctions example for other port settings)
+#define SerialAT Serial2
 
 #include <SimpleNBClient.h>
 #include <CRC32.h>
 
-// Just in case someone defined the wrong thing..
-#if SIMPLE_NB_USE_GPRS && not defined SIMPLE_NB_SUPPORT_GPRS
-#undef SIMPLE_NB_USE_GPRS
-#undef SIMPLE_NB_USE_WIFI
-#define SIMPLE_NB_USE_GPRS false
-#define SIMPLE_NB_USE_WIFI true
-#endif
-#if SIMPLE_NB_USE_WIFI && not defined SIMPLE_NB_SUPPORT_WIFI
-#undef SIMPLE_NB_USE_GPRS
-#undef SIMPLE_NB_USE_WIFI
-#define SIMPLE_NB_USE_GPRS true
-#define SIMPLE_NB_USE_WIFI false
+#define PWRKEY   23       // GPIO pin used for PWRKEY
+#define BAUD_RATE 115200  // Baud rate to be used for communicating with the modem
+
+// Increase RX buffer to capture the entire response. Some chips without internal
+// buffering need enough space in the buffer for the entire response, else data
+// will be lost (and the http library will fail.
+#if !defined(SIMPLE_NB_RX_BUFFER)
+#define SIMPLE_NB_RX_BUFFER 1024
 #endif
 
-const char resource[]    = "/SimpleNB/test_1k.bin";
-uint32_t   knownCRC32    = 0x6f50d767;
-uint32_t   knownFileSize = 1024;  // In case server does not send it
+// Server details
+const char server[]    = "vsh.pp.ua";
+const int port         = 80;
+const char resource[]  = "/TinyGSM/test_1k.bin";
+uint32_t knownCRC32    = 0x6f50d767;
+uint32_t knownFileSize = 1024;  // In case server does not send it
 
-#ifdef DUMP_AT_COMMANDS
-#include <StreamDebugger.h>
-StreamDebugger debugger(SerialAT, Serial);
-SimpleNB        modem(debugger);
-#else
-SimpleNB        modem(SerialAT);
-#endif
 
-SimpleNBClient client(modem);
+SimpleNB modem(SerialAT);
 
-void setup() {
-  // Set console baud rate
-  Serial.begin(115200);
-  delay(10);
+// different modem may have different time requirements, check datasheet of your modem
+void powerUp() {
+    digitalWrite(PWRKEY, LOW);
+    delay(1000);
+    digitalWrite(PWRKEY, HIGH);
+    delay(2500);
+}
 
-  // !!!!!!!!!!!
-  // Set your reset, enable, power pins here
-  // !!!!!!!!!!!
-
-  Serial.println("Wait...");
-
-  // Set GSM module baud rate
-  SerialAT.begin(115200);
-  delay(6000);
-
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  // modem.restart();
-  SimpleNBBegin(SerialAT, 115200);
-  Serial.println("Initializing modem...");
-  modem.init();
-
-  String modemInfo = modem.getModemInfo();
-  Serial.print("Modem Info: ");
-  Serial.println(modemInfo);
-
-#if SIMPLE_NB_USE_GPRS
-  // Unlock your SIM card with a PIN if needed
-  if (GSM_PIN && modem.getSimStatus() != 3) { modem.simUnlock(GSM_PIN); }
-#endif
+void powerDown() {
+    digitalWrite(PWRKEY, LOW);
+    delay(1200);
+    digitalWrite(PWRKEY, HIGH);
 }
 
 void printPercent(uint32_t readLength, uint32_t contentLength) {
-  // If we know the total length
-  if (contentLength != (uint32_t)-1) {
-    Serial.print("\r ");
-    Serial.print((100.0 * readLength) / contentLength);
-    Serial.print('%');
-  } else {
+    // If we know the total length
+    if (contentLength != (uint32_t)-1) {
+      Serial.print("\r ");
+      Serial.print((100.0 * readLength) / contentLength);
+      Serial.print('%');
+    } else {
+      Serial.println(readLength);
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(10);
+    Serial.println();
+
+    pinMode(PWRKEY, OUTPUT);
+    Serial.println("Powering Up...");
+    powerUp();
+
+    SimpleNBBegin(SerialAT, BAUD_RATE);
+    modem.init();
+
+    Serial.println("Waiting for network registration...");
+    if (!modem.waitForRegistration(60000L, true)) {
+      // can't register to the mobile network, check your sim or carrier setup
+      delay(1000);
+      return;
+    }
+
+    Serial.println("Activate Data Network...");
+    if (!modem.activateDataNetwork()) {
+      // can't connect to the data network, check your APN and sim setup
+      delay(1000);
+      return;
+    }
+
+    SimpleNBClient client(modem);
+
+    Serial.print(F("Connecting to "));
+    Serial.print(server);
+    if (!client.connect(server, port)) {
+      Serial.println(" fail");
+      delay(10000);
+      return;
+    }
+    Serial.println(" connected");
+
+    // Make a HTTP GET request:
+    client.println(String("GET ") + resource + " HTTP/1.1");
+    client.println(String("Host: ") + server);
+    client.println("Connection: close\r\n");
+
+    // Let's see what the entire elapsed time is, from after we send the request.
+    uint32_t timeElapsed = millis();
+
+    Serial.println(F("Waiting for response header"));
+
+    // While we are still looking for the end of the header (i.e. empty line
+    // FOLLOWED by a newline), continue to read data into the buffer, parsing each
+    // line (data FOLLOWED by a newline). Time out if it takes too long.
+
+    const uint32_t timeout   = 5000;
+    uint32_t       clientReadStartTime = millis();
+    String         headerBuffer;
+    bool           finishedHeader = false;
+    uint32_t       contentLength  = 0;
+
+    // loop through the incoming data until the end of http headers
+    while (!finishedHeader) {
+      int nlPos;
+
+      if (client.available()) {
+        clientReadStartTime = millis();
+        while (client.available()) {
+          char c = client.read();
+          headerBuffer += c;
+
+          // Uncomment the lines below to see the data coming into the buffer
+          // Serial.print((char) c);
+
+          if (headerBuffer.indexOf(F("\r\n")) >= 0) break;
+        }
+      } else {
+        if (millis() - clientReadStartTime > timeout) {
+          // Time-out waiting for data from client
+          Serial.println(F(">>> Client Timeout !"));
+          break;
+        }
+      }
+
+      // See if we have a new line.
+      nlPos = headerBuffer.indexOf(F("\r\n"));
+
+      // if "\r\n" is at the beginning of the line, it is the beginning of payload
+      if (nlPos > 0) {
+        // Look for the line contains Content-Length
+        if (headerBuffer.startsWith(F("Content-Length:"))) {
+          contentLength = headerBuffer.substring(headerBuffer.indexOf(':') + 1).toInt();
+        }
+
+        headerBuffer.remove(0, nlPos + 2);  // remove the line
+      } else if (nlPos == 0) {
+        finishedHeader = true;
+      }
+    }
+
+    // The two cases which are not managed properly are as follows:
+    // 1. The client doesn't provide data quickly enough to keep up with this loop.
+    // 2. If the client data is segmented in the middle of the 'Content-Length:' header,
+    //    then that header may be missed/damaged.
+
+    uint32_t readLength = 0;
+    CRC32    crc;
+
+    if (finishedHeader && contentLength == knownFileSize) {
+      Serial.println(F("Reading response data"));
+      clientReadStartTime = millis();
+
+      printPercent(readLength, contentLength);
+      while (readLength < contentLength && client.connected() &&
+             millis() - clientReadStartTime < timeout) {
+        while (client.available()) {
+          uint8_t c = client.read();
+          // Serial.print(reinterpret_cast<char>c);  // Uncomment this to show
+          // data
+          crc.update(c);
+          readLength++;
+          if (readLength % (contentLength / 13) == 0) {
+            printPercent(readLength, contentLength);
+          }
+          clientReadStartTime = millis();
+        }
+      }
+      printPercent(readLength, contentLength);
+    }
+
+    timeElapsed = millis() - timeElapsed;
+    Serial.println();
+
+    // Shutdown
+    client.stop();
+    Serial.println(F("Server disconnected"));
+    modem.deactivateDataNetwork();
+    Serial.println(F("Data Network deactivated"));
+
+    float duration = float(timeElapsed) / 1000;
+
+    Serial.println();
+    Serial.print("Content-Length: ");
+    Serial.println(contentLength);
+    Serial.print("Actually read:  ");
     Serial.println(readLength);
-  }
+    Serial.print("Calc. CRC32:    0x");
+    Serial.println(crc.finalize(), HEX);
+    Serial.print("Known CRC32:    0x");
+    Serial.println(knownCRC32, HEX);
+    Serial.print("Duration:       ");
+    Serial.print(duration);
+    Serial.println("s");
+
 }
 
 void loop() {
-#if SIMPLE_NB_USE_WIFI
-  // Wifi connection parameters must be set before waiting for the network
-  Serial.print(F("Setting SSID/password..."));
-  if (!modem.networkConnect(wifiSSID, wifiPass)) {
-    Serial.println(" fail");
-    delay(10000);
-    return;
-  }
-  Serial.println(" success");
-#endif
 
-#if SIMPLE_NB_USE_GPRS && defined SIMPLE_NB_MODEM_XBEE
-  // The XBee must run the gprsConnect function BEFORE waiting for network!
-  modem.gprsConnect(apn, gprsUser, gprsPass);
-#endif
-
-  Serial.print("Waiting for network...");
-  if (!modem.waitForRegistration()) {
-    Serial.println(" fail");
-    delay(10000);
-    return;
-  }
-  Serial.println(" success");
-
-  if (modem.isNetworkRegistered()) { Serial.println("Network registered"); }
-
-#if SIMPLE_NB_USE_GPRS
-  // GPRS connection parameters are usually set after network registration
-  Serial.print(F("Connecting to "));
-  Serial.print(apn);
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    Serial.println(" fail");
-    delay(10000);
-    return;
-  }
-  Serial.println(" success");
-
-  if (modem.isGprsConnected()) { Serial.println("GPRS connected"); }
-#endif
-
-  Serial.print(F("Connecting to "));
-  Serial.print(server);
-  if (!client.connect(server, port)) {
-    Serial.println(" fail");
-    delay(10000);
-    return;
-  }
-  Serial.println(" success");
-
-  // Make a HTTP GET request:
-  client.print(String("GET ") + resource + " HTTP/1.0\r\n");
-  client.print(String("Host: ") + server + "\r\n");
-  client.print("Connection: close\r\n\r\n");
-
-  // Let's see what the entire elapsed time is, from after we send the request.
-  uint32_t timeElapsed = millis();
-
-  Serial.println(F("Waiting for response header"));
-
-  // While we are still looking for the end of the header (i.e. empty line
-  // FOLLOWED by a newline), continue to read data into the buffer, parsing each
-  // line (data FOLLOWED by a newline). If it takes too long to get data from
-  // the client, we need to exit.
-
-  const uint32_t clientReadTimeout   = 5000;
-  uint32_t       clientReadStartTime = millis();
-  String         headerBuffer;
-  bool           finishedHeader = false;
-  uint32_t       contentLength  = 0;
-
-  while (!finishedHeader) {
-    int nlPos;
-
-    if (client.available()) {
-      clientReadStartTime = millis();
-      while (client.available()) {
-        char c = client.read();
-        headerBuffer += c;
-
-        // Uncomment the lines below to see the data coming into the buffer
-        // if (c < 16)
-        //   Serial.print('0');
-        // Serial.print(c, HEX);
-        // Serial.print(' ');
-        // if (isprint(c))
-        //   Serial.print(reinterpret_cast<char> c);
-        // else
-        //   Serial.print('*');
-        // Serial.print(' ');
-
-        // Let's exit and process if we find a new line
-        if (headerBuffer.indexOf(F("\r\n")) >= 0) break;
-      }
-    } else {
-      if (millis() - clientReadStartTime > clientReadTimeout) {
-        // Time-out waiting for data from client
-        Serial.println(F(">>> Client Timeout !"));
-        break;
-      }
-    }
-
-    // See if we have a new line.
-    nlPos = headerBuffer.indexOf(F("\r\n"));
-
-    if (nlPos > 0) {
-      headerBuffer.toLowerCase();
-      // Check if line contains content-length
-      if (headerBuffer.startsWith(F("content-length:"))) {
-        contentLength =
-            headerBuffer.substring(headerBuffer.indexOf(':') + 1).toInt();
-        // Serial.print(F("Got Content Length: "));  // uncomment for
-        // Serial.println(contentLength);            // confirmation
-      }
-
-      headerBuffer.remove(0, nlPos + 2);  // remove the line
-    } else if (nlPos == 0) {
-      // if the new line is empty (i.e. "\r\n" is at the beginning of the line),
-      // we are done with the header.
-      finishedHeader = true;
-    }
-  }
-
-  // The two cases which are not managed properly are as follows:
-  // 1. The client doesn't provide data quickly enough to keep up with this
-  // loop.
-  // 2. If the client data is segmented in the middle of the 'Content-Length: '
-  // header,
-  //    then that header may be missed/damaged.
-  //
-
-  uint32_t readLength = 0;
-  CRC32    crc;
-
-  if (finishedHeader && contentLength == knownFileSize) {
-    Serial.println(F("Reading response data"));
-    clientReadStartTime = millis();
-
-    printPercent(readLength, contentLength);
-    while (readLength < contentLength && client.connected() &&
-           millis() - clientReadStartTime < clientReadTimeout) {
-      while (client.available()) {
-        uint8_t c = client.read();
-        // Serial.print(reinterpret_cast<char>c);  // Uncomment this to show
-        // data
-        crc.update(c);
-        readLength++;
-        if (readLength % (contentLength / 13) == 0) {
-          printPercent(readLength, contentLength);
-        }
-        clientReadStartTime = millis();
-      }
-    }
-    printPercent(readLength, contentLength);
-  }
-
-  timeElapsed = millis() - timeElapsed;
-  Serial.println();
-
-  // Shutdown
-
-  client.stop();
-  Serial.println(F("Server disconnected"));
-
-#if SIMPLE_NB_USE_WIFI
-  modem.networkDisconnect();
-  Serial.println(F("WiFi disconnected"));
-#endif
-#if SIMPLE_NB_USE_GPRS
-  modem.gprsDisconnect();
-  Serial.println(F("GPRS disconnected"));
-#endif
-
-  float duration = float(timeElapsed) / 1000;
-
-  Serial.println();
-  Serial.print("Content-Length: ");
-  Serial.println(contentLength);
-  Serial.print("Actually read:  ");
-  Serial.println(readLength);
-  Serial.print("Calc. CRC32:    0x");
-  Serial.println(crc.finalize(), HEX);
-  Serial.print("Known CRC32:    0x");
-  Serial.println(knownCRC32, HEX);
-  Serial.print("Duration:       ");
-  Serial.print(duration);
-  Serial.println("s");
-
-  // Do nothing forevermore
-  while (true) { delay(1000); }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,42 @@
 /**************************************************************
  *
- * For this example, you need to install PubSubClient library:
- *   https://github.com/knolleary/pubsubclient, or from
- *   http://librarymanager/all#PubSubClient
+ * For this example, you need to install CRC32 library:
+ *   https://github.com/bakercp/CRC32
+ *   or from http://librarymanager/all#CRC32+checksum
  *
- * This example is mainly to show the pre-requisition that is
- * needed for establishing the NB-IoT connect in the setup().
- * For the rest of the part, it is from PubSubClient. For more
- * MQTT examples, see PubSubClient library.
- *
- **************************************************************
- * This example connects to HiveMQ's showcase broker.
- *
- * Test sending and receiving messages from the HiveMQ webclient
- * available at http://www.hivemq.com/demos/websocket-client/.
- *
- * Publish anything to the topic SimpleNB/led will toggle the
- * on-board LED,
- * Subscribe to the topic SimpleNB/status to receive LED status
- * send back by the mqtt client after the toggling.
+ * ATTENTION! Downloading big files requires of knowledge of
+ * the SimpleNB internals and some modem specifics,
+ * so this is for more experienced developers.
  *
  **************************************************************/
-
 // Select your modem: (See AllFunctions example for the definition of other modem)
 #define SIMPLE_NB_MODEM_SIM7080
-
-// Enbale Serial.print debug output to Serial Monitor for debug prints, if needed
-//#define SIMPLE_NB_DEBUG Serial
 
 // Select your Serial port for AT interface (See AllFunctions example for other port settings)
 #define SerialAT Serial2
 
 #include <SimpleNBClient.h>
-#include <PubSubClient.h>
+#include <CRC32.h>
 
-#define LED_PIN   2
-#define PWRKEY    23      // GPIO pin used for PWRKEY
+#define PWRKEY   23       // GPIO pin used for PWRKEY
 #define BAUD_RATE 115200  // Baud rate to be used for communicating with the modem
 
-// MQTT details
-const char* broker      = "broker.hivemq.com";
-const char* topicLed    = "SimpleNB/led";
-const char* topicStatus = "SimpleNB/status";
+// Increase RX buffer to capture the entire response. Some chips without internal
+// buffering need enough space in the buffer for the entire response, else data
+// will be lost (and the http library will fail.
+#if !defined(SIMPLE_NB_RX_BUFFER)
+#define SIMPLE_NB_RX_BUFFER 1024
+#endif
 
-SimpleNB       modem(SerialAT);
-SimpleNBClient client(modem);
-PubSubClient   mqtt(client);
+// Server details
+const char server[]    = "vsh.pp.ua";
+const int port         = 80;
+const char resource[]  = "/TinyGSM/test_1k.bin";
+uint32_t knownCRC32    = 0x6f50d767;
+uint32_t knownFileSize = 1024;  // In case server does not send it
 
-void mqttCallback(char* topic, byte* payload, unsigned int len) {
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("]: ");
-    for (int i=0; i < len; i++) {
-      Serial.print((char)payload[i]);
-    }
-    Serial.println();
 
-    // Only proceed if incoming message's topic matches
-    if (String(topic) == topicLed) {
-      uint8_t ledStatus = !digitalRead(LED_PIN);
-      digitalWrite(LED_PIN, ledStatus);
-      mqtt.publish(topicStatus, ledStatus ? "status=1" : "status=0");
-    }
-}
-
-void mqttConnect() {
-
-    while (!client.connected()) {
-        Serial.print("Connecting to ");
-        Serial.print(broker);
-        if (mqtt.connect("SimpleNBClient")) {
-        // if (mqtt.connect("SimpleNBClient", "mqtt_user", "mqtt_pass")) {
-            Serial.println(" connected");
-
-            const char* initMsg = "MQTT Started";
-            mqtt.publish(topicStatus, initMsg);
-            Serial.print("Publish topic [");
-            Serial.print(topicStatus);
-            Serial.print("]: ");
-            Serial.println(initMsg);
-
-            mqtt.subscribe(topicLed);
-            Serial.print("Subscribe to topic [");
-            Serial.print(topicLed);
-            Serial.println("]");
-        } else {
-            Serial.print(" fail, rc=");
-            Serial.print(mqtt.state());
-            Serial.println(" try again in 5 seconds");
-            delay(5000);
-        }
-    }
-}
+SimpleNB modem(SerialAT);
 
 // different modem may have different time requirements, check datasheet of your modem
 void powerUp() {
@@ -101,55 +46,175 @@ void powerUp() {
     delay(2500);
 }
 
-void modemConnect() {
-    Serial.print("Network registration... ");
-    if (!modem.waitForRegistration()) {
-      Serial.println("fail");
-      delay(1000);
-      return;
-    }
-    Serial.println("success");
+void powerDown() {
+    digitalWrite(PWRKEY, LOW);
+    delay(1200);
+    digitalWrite(PWRKEY, HIGH);
+}
 
-    Serial.print("Activate Data Network... ");
-    modem.deactivateDataNetwork();    // disconnect any broken connection if there is one
-    if (!modem.activateDataNetwork()) {
-      Serial.println("fail");
-      delay(1000);
-      return;
+void printPercent(uint32_t readLength, uint32_t contentLength) {
+    // If we know the total length
+    if (contentLength != (uint32_t)-1) {
+      Serial.print("\r ");
+      Serial.print((100.0 * readLength) / contentLength);
+      Serial.print('%');
+    } else {
+      Serial.println(readLength);
     }
-    Serial.println("success");
 }
 
 void setup() {
     Serial.begin(115200);
+    delay(10);
     Serial.println();
 
-    pinMode(LED_PIN, OUTPUT);
-
-    Serial.println("Powering Up...");
     pinMode(PWRKEY, OUTPUT);
+    Serial.println("Powering Up...");
     powerUp();
 
-    Serial.println("Initializing modem...");
     SimpleNBBegin(SerialAT, BAUD_RATE);
     modem.init();
 
-    modemConnect();
+    Serial.println("Waiting for network registration...");
+    if (!modem.waitForRegistration(60000L, true)) {
+      // can't register to the mobile network, check your sim or carrier setup
+      delay(1000);
+      return;
+    }
 
-    // MQTT Broker setup
-    mqtt.setServer(broker, 1883);
-    mqtt.setCallback(mqttCallback);
-    mqtt.setKeepAlive(60);
+    Serial.println("Activate Data Network...");
+    if (!modem.activateDataNetwork()) {
+      // can't connect to the data network, check your APN and sim setup
+      delay(1000);
+      return;
+    }
+
+    SimpleNBClient client(modem);
+
+    Serial.print(F("Connecting to "));
+    Serial.print(server);
+    if (!client.connect(server, port)) {
+      Serial.println(" fail");
+      delay(10000);
+      return;
+    }
+    Serial.println(" connected");
+
+    // Make a HTTP GET request:
+    client.println(String("GET ") + resource + " HTTP/1.1");
+    client.println(String("Host: ") + server);
+    client.println("Connection: close\r\n");
+
+    // Let's see what the entire elapsed time is, from after we send the request.
+    uint32_t timeElapsed = millis();
+
+    Serial.println(F("Waiting for response header"));
+
+    // While we are still looking for the end of the header (i.e. empty line
+    // FOLLOWED by a newline), continue to read data into the buffer, parsing each
+    // line (data FOLLOWED by a newline). Time out if it takes too long.
+
+    const uint32_t timeout   = 5000;
+    uint32_t       clientReadStartTime = millis();
+    String         headerBuffer;
+    bool           finishedHeader = false;
+    uint32_t       contentLength  = 0;
+
+    // loop through the incoming data until the end of http headers
+    while (!finishedHeader) {
+      int nlPos;
+
+      if (client.available()) {
+        clientReadStartTime = millis();
+        while (client.available()) {
+          char c = client.read();
+          headerBuffer += c;
+
+          // Uncomment the lines below to see the data coming into the buffer
+          // Serial.print((char) c);
+
+          if (headerBuffer.indexOf(F("\r\n")) >= 0) break;
+        }
+      } else {
+        if (millis() - clientReadStartTime > timeout) {
+          // Time-out waiting for data from client
+          Serial.println(F(">>> Client Timeout !"));
+          break;
+        }
+      }
+
+      // See if we have a new line.
+      nlPos = headerBuffer.indexOf(F("\r\n"));
+
+      // if "\r\n" is at the beginning of the line, it is the beginning of payload
+      if (nlPos > 0) {
+        // Look for the line contains Content-Length
+        if (headerBuffer.startsWith(F("Content-Length:"))) {
+          contentLength = headerBuffer.substring(headerBuffer.indexOf(':') + 1).toInt();
+        }
+
+        headerBuffer.remove(0, nlPos + 2);  // remove the line
+      } else if (nlPos == 0) {
+        finishedHeader = true;
+      }
+    }
+
+    // The two cases which are not managed properly are as follows:
+    // 1. The client doesn't provide data quickly enough to keep up with this loop.
+    // 2. If the client data is segmented in the middle of the 'Content-Length:' header,
+    //    then that header may be missed/damaged.
+
+    uint32_t readLength = 0;
+    CRC32    crc;
+
+    if (finishedHeader && contentLength == knownFileSize) {
+      Serial.println(F("Reading response data"));
+      clientReadStartTime = millis();
+
+      printPercent(readLength, contentLength);
+      while (readLength < contentLength && client.connected() &&
+             millis() - clientReadStartTime < timeout) {
+        while (client.available()) {
+          uint8_t c = client.read();
+          // Serial.print(reinterpret_cast<char>c);  // Uncomment this to show
+          // data
+          crc.update(c);
+          readLength++;
+          if (readLength % (contentLength / 13) == 0) {
+            printPercent(readLength, contentLength);
+          }
+          clientReadStartTime = millis();
+        }
+      }
+      printPercent(readLength, contentLength);
+    }
+
+    timeElapsed = millis() - timeElapsed;
+    Serial.println();
+
+    // Shutdown
+    client.stop();
+    Serial.println(F("Server disconnected"));
+    modem.deactivateDataNetwork();
+    Serial.println(F("Data Network deactivated"));
+
+    float duration = float(timeElapsed) / 1000;
+
+    Serial.println();
+    Serial.print("Content-Length: ");
+    Serial.println(contentLength);
+    Serial.print("Actually read:  ");
+    Serial.println(readLength);
+    Serial.print("Calc. CRC32:    0x");
+    Serial.println(crc.finalize(), HEX);
+    Serial.print("Known CRC32:    0x");
+    Serial.println(knownCRC32, HEX);
+    Serial.print("Duration:       ");
+    Serial.print(duration);
+    Serial.println("s");
+
 }
 
 void loop() {
-    if ( !(client.connected() || modem.isNetworkRegistered()) ) {
-      modemConnect();
-    }
 
-    if (!mqtt.connected()) {
-      mqttConnect();
-    }
-
-    mqtt.loop();
 }
