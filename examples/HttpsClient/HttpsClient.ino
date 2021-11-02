d57f05b09c3e123d9f144803eed46c9f754d6b7c
Henry Cheung <henrycheung@techstudio.design>
2021-11-02 17:20:04 +0800

    updating HttpsClient(not complete yet)

--- examples/HttpsClient/HttpsClient.ino
+++ examples/HttpsClient/HttpsClient.ino
@@ -22,9 +22,12 @@
  **************************************************************/
 
 // Select your modem:
-#define SIMPLE_NB_MODEM_SIM7000
+// SSL/TLS is not yet implemented on the Quectel BG96 and SIM7020
+// #define SIMPLE_NB_MODEM_SIM7000
 // #define SIMPLE_NB_MODEM_SIM7000SSL
-// #define SIMPLE_NB_MODEM_SIM7080
+// #define SIMPLE_NB_MODEM_SIM7070
+#define SIMPLE_NB_MODEM_SIM7080
+// #define SIMPLE_NB_MODEM_SIM7090
 // #define SIMPLE_NB_MODEM_UBLOX
 // #define SIMPLE_NB_MODEM_SARAR4
 // #define SIMPLE_NB_MODEM_BG96
@@ -50,142 +53,76 @@ SoftwareSerial SerialAT(2, 3);  // RX, TX
 #define SIMPLE_NB_RX_BUFFER 650
 #endif
 
-// See all AT commands, if wanted
-// #define DUMP_AT_COMMANDS
-
-// Define the serial console for debug prints, if needed
+// Enbale DBG debug output to Serial Monitor for debug prints, if needed
 #define SIMPLE_NB_DEBUG Serial
-// #define LOGGING  // <- Logging is for the HTTP library
 
-// Add a reception delay, if needed.
-// This may be needed for a fast processor at a slow baud rate.
-// #define SIMPLE_NB_YIELD() { delay(2); }
-
-// Define how you're planning to connect to the internet.
-// This is only needed for this example, not in other code.
-#define SIMPLE_NB_USE_GPRS true
-#define SIMPLE_NB_USE_WIFI false
-
-// set GSM PIN, if any
-#define GSM_PIN ""
-
-// flag to force SSL client authentication, if needed
-// #define SIMPLE_NB_SSL_CLIENT_AUTHENTICATION
-
-// Your GPRS credentials, if any
-const char apn[]      = "YourAPN";
-const char gprsUser[] = "";
-const char gprsPass[] = "";
-
-// Your WiFi connection credentials, if applicable
-const char wifiSSID[] = "YourSSID";
-const char wifiPass[] = "YourWiFiPass";
+#define PWRKEY    23      // GPIO pin used for PWRKEY
+#define BAUD_RATE 115200  // Baud rate to be used for communicating with the modem
 
 // Server details
-const char server[]   = "vsh.pp.ua";
-const char resource[] = "/TinyGSM/logo.txt";
+const char server[]   = "httpbin.org";
+const char resource[] = "/post";
 const int  port       = 443;
 
 #include <SimpleNBClient.h>
-#include <ArduinoHttpClient.h>
-
-// Just in case someone defined the wrong thing..
-#if SIMPLE_NB_USE_GPRS && not defined SIMPLE_NB_SUPPORT_GPRS
-#undef SIMPLE_NB_USE_GPRS
-#undef SIMPLE_NB_USE_WIFI
-#define SIMPLE_NB_USE_GPRS false
-#define SIMPLE_NB_USE_WIFI true
-#endif
-#if SIMPLE_NB_USE_WIFI && not defined SIMPLE_NB_SUPPORT_WIFI
-#undef SIMPLE_NB_USE_GPRS
-#undef SIMPLE_NB_USE_WIFI
-#define SIMPLE_NB_USE_GPRS true
-#define SIMPLE_NB_USE_WIFI false
-#endif
-
-#ifdef DUMP_AT_COMMANDS
-#include <StreamDebugger.h>
-StreamDebugger debugger(SerialAT, Serial);
-SimpleNB        modem(debugger);
-#else
-SimpleNB        modem(SerialAT);
-#endif
 
 SimpleNBClientSecure client(modem);
-HttpClient          http(client, server, port);
+HttpClient http(client, server, port);
+
+// different modem may have different time requirements, check datasheet of your modem
+void powerUp() {
+    digitalWrite(PWRKEY, LOW);
+    delay(1000);
+    digitalWrite(PWRKEY, HIGH);
+    delay(2500);
+}
+
+void powerDown() {
+    digitalWrite(PWRKEY, LOW);
+    delay(1200);
+    digitalWrite(PWRKEY, HIGH);
+}
 
 void setup() {
-  // Set console baud rate
   Serial.begin(115200);
-  delay(10);
-
-  // !!!!!!!!!!!
-  // Set your reset, enable, power pins here
-  // !!!!!!!!!!!
-
-  Serial.println("Wait...");
-
-  // Set GSM module baud rate
-  SimpleNBBegin(SerialAT, 115200);
-  //SimpleNBAutoBaud(SerialAT, 9600, 115200);
-  delay(6000);
-
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  Serial.println("Initializing modem...");
-  modem.restart();
-  // modem.init();
-
-  String modemInfo = modem.getModemInfo();
-  Serial.print("Modem Info: ");
-  Serial.println(modemInfo);
-
-#if SIMPLE_NB_USE_GPRS
-  // Unlock your SIM card with a PIN if needed
-  if (GSM_PIN && modem.getSimStatus() != 3) { modem.simUnlock(GSM_PIN); }
-#endif
+  digitalWrite(PWRKEY, HIGH);   // Keep PWRKEY off
+  pinMode(PWRKEY, OUTPUT);
 }
 
 void loop() {
-#if SIMPLE_NB_USE_WIFI
-  // Wifi connection parameters must be set before waiting for the network
-  Serial.print(F("Setting SSID/password..."));
-  if (!modem.networkConnect(wifiSSID, wifiPass)) {
-    Serial.println(" fail");
-    delay(10000);
-    return;
-  }
-  Serial.println(" success");
-#endif
 
-#if SIMPLE_NB_USE_GPRS && defined SIMPLE_NB_MODEM_XBEE
-  // The XBee must run the gprsConnect function BEFORE waiting for network!
-  modem.gprsConnect(apn, gprsUser, gprsPass);
-#endif
+  SimpleNBBegin(SerialAT, 115200);
 
-  Serial.print("Waiting for network...");
+  Serial.println("Initializing modem...");
+  modem.init();
+
+  Serial.print("Waiting for network registration... ");
   if (!modem.waitForRegistration()) {
-    Serial.println(" fail");
+    Serial.println("fail");
     delay(10000);
     return;
   }
-  Serial.println(" success");
+  Serial.println("registered");
 
-  if (modem.isNetworkRegistered()) { Serial.println("Network connected"); }
-
-#if SIMPLE_NB_USE_GPRS
-  // GPRS connection parameters are usually set after network registration
-  Serial.print(F("Connecting to "));
-  Serial.print(apn);
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    Serial.println(" fail");
-    delay(10000);
+  DBG("Activate Data Network...");
+  if (!modem.activateDataNetwork()) {
+    delay(1000);
     return;
   }
-  Serial.println(" success");
 
-  if (modem.isGprsConnected()) { Serial.println("GPRS connected"); }
-#endif
+// #if SIMPLE_NB_USE_GPRS
+//   // GPRS connection parameters are usually set after network registration
+//   Serial.print(F("Connecting to "));
+//   Serial.print(apn);
+//   if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+//     Serial.println(" fail");
+//     delay(10000);
+//     return;
+//   }
+//   Serial.println(" success");
+//
+//   if (modem.isGprsConnected()) { Serial.println("GPRS connected"); }
+// #endif
 
   Serial.print(F("Performing HTTPS GET request... "));
   http.connectionKeepAlive();  // Currently, this is needed for HTTPS
@@ -232,15 +169,10 @@ void loop() {
   http.stop();
   Serial.println(F("Server disconnected"));
 
-#if SIMPLE_NB_USE_WIFI
-  modem.networkDisconnect();
-  Serial.println(F("WiFi disconnected"));
-#endif
 #if SIMPLE_NB_USE_GPRS
   modem.gprsDisconnect();
   Serial.println(F("GPRS disconnected"));
 #endif
 
-  // Do nothing forevermore
-  while (true) { delay(1000); }
+  delay(120000);
 }
